59fca49b84b6241020bc893072878d32dc2040c1
jomokpe-coder <jomokpe@gmail.com>
2025-11-27 09:27:27 +0100

    Update Delivery.js

--- Delivery.js
+++ Delivery.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 
-export default function Delivery(){
+export default function Delivery() {
   return (
-    <div className="page container">
+    <div className="page">
       <h1>Delivery Form</h1>
-      <p>Fill out the form below to schedule your shipment.</p>
-      <a className="primary-btn" href="#form">Place Delivery Request</a>
-      <div id="form" style={marginTop:20}>
-        <iframe 
-          src="https://docs.google.com/forms/d/e/1FAIpQLScV2OkbcHexdx8EdxIh7WQX3UvXV0MHuWom7GW9PR3wsvCCdQ/viewform?embedded=true"
-          title="Delivery Form"
-          width="100%" height="1600" frameBorder="0" marginHeight="0" marginWidth="0"
-        >
+
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScV2OkbcHexdx8EdxIh7WQX3UvXV0MHuWom7GW9PR3wsvCCdQ/viewform?embedded=true"
+        width="100%"
+        height="2000"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        style={{ border: "none" }}
+      >
         Loading…
-        </iframe>
-      </div>
+      </iframe>
     </div>
   );
 }
