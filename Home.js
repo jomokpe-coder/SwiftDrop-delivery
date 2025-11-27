8b66200140c780fce35616a9132ba1adef02dbf4
jomokpe-coder <jomokpe@gmail.com>
2025-11-27 09:08:44 +0100

    Update Home.js

--- Home.js
+++ Home.js
@@ -5,7 +5,22 @@ export default function Home(){
     <div className="page container">
       <h1>Welcome to SwiftDrop Delivery</h1>
       <p>Fast • Reliable • Secure</p>
-      <a className="primary-btn" href="/delivery">Place Delivery Request</a>
+
+      {/* Existing button (Delivery Page) */}
+      <a className="primary-btn" href="/delivery">
+        Go to Delivery Page
+      </a>
+
+      {/* NEW Google Form Button */}
+      <a
+        className="primary-btn"
+        href="https://docs.google.com/forms/d/e/1FAIpQLScV2OkbcHexdx8EdxIh7WQX3UvXV0MHuWom7GW9PR3wsvCCdQ/viewform?embedded=true"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ marginTop: "20px", display: "inline-block" }}
+      >
+        Fill Delivery Form
+      </a>
     </div>
   );
 }
