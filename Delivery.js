b6607b20aac1d116f28ba16bbbc17ba4379dcd65
jomokpe-coder <jomokpe@gmail.com>
2025-11-27 09:37:58 +0100

    Add files via upload

--- Delivery.js
+++ Delivery.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React from 'react';
 
-export default function Delivery() {
+export default function Delivery(){
   return (
-    <div className="page">
+    <div className="page container">
       <h1>Delivery Form</h1>
-
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLScV2OkbcHexdx8EdxIh7WQX3UvXV0MHuWom7GW9PR3wsvCCdQ/viewform?embedded=true"
-        width="100%"
-        height="2000"
-        frameBorder="0"
-        marginHeight="0"
-        marginWidth="0"
-        style={{ border: "none" }}
-      >
+      <p>Fill out the form below to schedule your shipment.</p>
+      <a className="primary-btn" href="#form">Place Delivery Request</a>
+      <div id="form" style={marginTop:20}>
+        <iframe 
+          src="https://docs.google.com/forms/d/e/1FAIpQLScV2OkbcHexdx8EdxIh7WQX3UvXV0MHuWom7GW9PR3wsvCCdQ/viewform?embedded=true"
+          title="Delivery Form"
+          width="100%" height="1600" frameBorder="0" marginHeight="0" marginWidth="0"
+        >
         Loading…
-      </iframe>
+        </iframe>
+      </div>
     </div>
   );
 }
--- Home.js
+++ Home.js
@@ -5,22 +5,7 @@ export default function Home(){
     <div className="page container">
       <h1>Welcome to SwiftDrop Delivery</h1>
       <p>Fast • Reliable • Secure</p>
-
-      {/* Existing button (Delivery Page) */}
-      <a className="primary-btn" href="/delivery">
-        Go to Delivery Page
-      </a>
-
-      {/* NEW Google Form Button */}
-      <a
-        className="primary-btn"
-        href="https://docs.google.com/forms/d/e/1FAIpQLScV2OkbcHexdx8EdxIh7WQX3UvXV0MHuWom7GW9PR3wsvCCdQ/viewform?embedded=true"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ marginTop: "20px", display: "inline-block" }}
-      >
-        Fill Delivery Form
-      </a>
+      <a className="primary-btn" href="/delivery">Place Delivery Request</a>
     </div>
   );
 }
